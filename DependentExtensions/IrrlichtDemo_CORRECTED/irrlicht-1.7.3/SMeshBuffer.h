b88c9520f8d9c243ff5b48f9a377596ae365b334
consultit <consultit@katamail.com>
2015-03-06 12:15:45 +0100

    IrrlichtDemo fully integrated.

--- DependentExtensions/IrrlichtDemo_CORRECTED/irrlicht-1.7.3/SMeshBuffer.h
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2002-2010 Nikolaus Gebhardt
-// This file is part of the "Irrlicht Engine".
-// For conditions of distribution and use, see copyright notice in irrlicht.h
-
-// replaced by template
-#include "../../IrrlichtDemo_CORRECTED/irrlicht-1.7.3/CMeshBuffer.h"
-
